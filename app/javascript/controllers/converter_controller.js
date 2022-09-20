2627ced1cae25e52435028d32fdec66c90c94e5a
Marco Roth <marco.roth@intergga.ch>
2022-09-20 22:03:09 +0200

    Save last input to `sessionStorage`

--- app/javascript/controllers/converter_controller.js
+++ app/javascript/controllers/converter_controller.js
@@ -6,9 +6,23 @@ export default class extends Controller {
 
   connect() {
     this.outputTarget.classList.add("hidden")
+
+    if (this.inputTarget.value.trim() === "") {
+      this.inputTarget.value = this.sessionStorageValue
+      this.convert()
+    }
   }
 
   convert() {
+    this.save()
     this.formTarget.requestSubmit()
+  }
+
+  save() {
+    sessionStorage.setItem("input", this.inputTarget.value)
+  }
+
+  get sessionStorageValue() {
+    return sessionStorage.getItem("input")
   }
 }
