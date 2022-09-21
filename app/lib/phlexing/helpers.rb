22455832dc3ea8f77e07e88427fc4b8075b00aa6
Marco Roth <marco.roth@intergga.ch>
2022-09-22 00:59:42 +0200

    Add more helpers

--- app/lib/phlexing/helpers.rb
+++ app/lib/phlexing/helpers.rb
@@ -20,5 +20,43 @@ module Phlexing
     def do_block_end(level = 0)
       indent(level) + "end\n"
     end
+
+    def multi_line_block(level)
+      @buffer << " do\n"
+      yield
+      @buffer << indent(level) + "end\n"
+    end
+
+    def single_line_block
+      @buffer << " { "
+      yield
+      @buffer << " }\n"
+    end
+
+    def erb_node?(node)
+      node.is_a?(Nokogiri::XML::Element) && node.name == "erb"
+    end
+
+    def text_node?(node)
+      node.is_a?(Nokogiri::XML::Text)
+    end
+
+    def erb_interpolation?(node)
+      first = node.children.first
+
+      erb_node?(node) &&
+        node.children.one? &&
+        first.children.none? &&
+        text_node?(first) &&
+        node.attributes["interpolated"]
+    end
+
+    def erb_safe_output?(node)
+      erb_interpolation?(node) && node.text.starts_with?('=')
+    end
+
+    def erb_comment?(node)
+      node.attributes["comment"]
+    end
   end
 end
