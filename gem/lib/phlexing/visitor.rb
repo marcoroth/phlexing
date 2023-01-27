ae65d0b9bd39b3ff202b44753bb2d40895ac4463
Marco Roth <marco.roth@intergga.ch>
2023-01-27 13:47:17 +0100

    rename `@converter` -> `@analyzer` in `Phlexing::Visitor`

--- gem/lib/phlexing/visitor.rb
+++ gem/lib/phlexing/visitor.rb
@@ -6,20 +6,20 @@ module Phlexing
   class Visitor < SyntaxTree::Visitor
     using Refinements::StringRefinements
 
-    def initialize(converter)
-      @converter = converter
+    def initialize(analyzer)
+      @analyzer = analyzer
     end
 
     def visit_ivar(node)
-      @converter.ivars << node.value.from(1)
+      @analyzer.ivars << node.value.from(1)
     end
 
     def visit_const(node)
-      @converter.consts << node.value
+      @analyzer.consts << node.value
     end
 
     def visit_command(node)
-      @converter.instance_methods << node.message.value
+      @analyzer.instance_methods << node.message.value
       super
     end
 
@@ -31,37 +31,37 @@ module Phlexing
 
           case node.receiver.value
           when SyntaxTree::IVar
-            @converter.ivars << value.from(1)
+            @analyzer.ivars << value.from(1)
           when SyntaxTree::Ident
-            @converter.idents << value
+            @analyzer.idents << value
           end
 
-          @converter.calls << value
+          @analyzer.calls << value
 
         when SyntaxTree::VCall
           case node.receiver.value
           when SyntaxTree::Ident
-            @converter.calls << node.receiver.value.value
+            @analyzer.calls << node.receiver.value.value
           end
 
         when SyntaxTree::Ident
           value = node.receiver.value.value.value
 
-          @converter.idents << value unless value.ends_with?("?")
-          @converter.calls << value
+          @analyzer.idents << value unless value.ends_with?("?")
+          @analyzer.calls << value
 
         when SyntaxTree::Const
-          @converter.calls << node.receiver.value
+          @analyzer.calls << node.receiver.value
         end
 
       elsif node.receiver.nil? && node.operator.nil?
         case node.message
         when SyntaxTree::Ident
           if node.message.value.end_with?("?") || node.child_nodes[3].is_a?(SyntaxTree::ArgParen)
-            @converter.instance_methods << node.message.value
-            @converter.calls << node.message.value
+            @analyzer.instance_methods << node.message.value
+            @analyzer.calls << node.message.value
           else
-            @converter.idents << node.message.value
+            @analyzer.idents << node.message.value
           end
         end
       end
@@ -70,11 +70,11 @@ module Phlexing
     end
 
     def visit_vcall(node)
-      @converter.locals << node.value.value
+      @analyzer.locals << node.value.value
     end
 
     def visit_ident(node)
-      @converter.idents << node.value
+      @analyzer.idents << node.value
     end
   end
 end
